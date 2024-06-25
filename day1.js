14ce81e8762759a6053340c4edf279278200f140
maheshbasnet089 <basnetmanish089@gmail.com>
2024-06-25 14:27:20 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,32 @@
+// const name = 'Manish Basnet'
+const name2 = "Aaashish pokhrel"
+const name3 = 'Pranjal Sharma'
+
+
+const names = ['Manish Basnet', 'Aashish Pokhrel','Pranjal Sharma']
+// names.push('Hello world')
+// names.pop()
+// names.shift()
+// names.unshift('Hello world')
+// console.log(names.length)
+// console.log(names)
+
+
+
+// const name = "Manish Basnet "
+// const age = 22 
+// const address = "Itahari"
+// const location = "Balgram"
+
+
+
+const me = {
+    name : "Manish Basnet",
+    age : 22, 
+    address : "Itahari",
+    location : "Balgram"
+}
+me.faculty = 'BCA'
+console.log(me)
+// console.log(me.age)
+// console.log(me['age'])
